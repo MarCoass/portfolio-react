cd3409cd4b7ad2e9015aad64d470bd24bad05b1d
Martina Coassin <martycoassin@gmail.com>
2023-12-22 14:49:00 -0300

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,12 +4,13 @@ import { ButtonGroup, Button, Typography } from "@material-tailwind/react";
 export default function AboutMe() {
   return (
     <div className="flex justify-center flex-col">
-      <Typography variant="h4">¡Hola!</Typography>
+      <Typography variant="h2" className="text-rose-700 dark:text-rose-500">¡Hola, soy Martina!</Typography>
       <div className="flex justify-between ">
-        <div className="my-auto ">
-          <h3>Martina Coassin-Fernandez</h3>
-          <p>Tecnica Universitaria en Desarrollo Web</p>
-          <p className="mr-36">
+        <div className="">
+          <Typography variant="h6" className="text-rose-600 dark:text-rose-400">
+            Tecnica Universitaria en Desarrollo Web
+          </Typography>
+          <p className="mr-36 my-5">
             Soy una desarrolladora web de de Argentina. Buscando aprender nuevas
             tecnologias y lenguajes. Disfruto tanto del desafío del backend como
             de la creatividad del frontend. Estoy en la búsqueda de mi primer
@@ -17,19 +18,20 @@ export default function AboutMe() {
             me permitan crecer y seguir aprendiendo.
           </p>
           <div>
-            <h4>Contacto</h4>
             <ButtonGroup>
-              <Button>Linkedin</Button>
-              <Button>GitHub</Button>
-              <Button>Correo</Button>
-              <Button>CV</Button>
+              <Button className="bg-rose-500 hover:bg-rose-600 active:bg-rose-700 dark:bg-rose-400 dark:hover:bg-rose-300 dark:active:bg-rose-500 dark:text-rose-950">
+                Linkedin
+              </Button>
+              <Button className="bg-rose-500 hover:bg-rose-600 active:bg-rose-700 dark:bg-rose-400 dark:hover:bg-rose-300 dark:active:bg-rose-500 dark:text-rose-950">GitHub</Button>
+              <Button className="bg-rose-500 hover:bg-rose-600 active:bg-rose-700 dark:bg-rose-400 dark:hover:bg-rose-300 dark:active:bg-rose-500 dark:text-rose-950">Correo</Button>
+              <Button className="bg-rose-500 hover:bg-rose-600 active:bg-rose-700 dark:bg-rose-400 dark:hover:bg-rose-300 dark:active:bg-rose-500 dark:text-rose-950">CV</Button>
             </ButtonGroup>
           </div>
         </div>
 
         <img
           alt="foto de perfil"
-          className="h-60 w-60 rounded-full object-cover object-center"
+          className="h-60 w-60 rounded-full object-cover object-center shadow-xl shadow-rose-500 "
           src="https://avatars.githubusercontent.com/u/76189888?v=4"
         />
       </div>
