40febe067a781aec38b99d5b0cbf4f505a2294ea
Martina Coassin <martycoassin@gmail.com>
2023-12-22 16:08:13 -0300

    iconos agregados

--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -5,36 +5,36 @@ const tecnologias = [
   { nombre: "tailwindcss", svg: "tailwindcss.svg", desde: "2023" },
   { nombre: "nextjs", svg: "nextjs.svg", desde: "2023" },
   { nombre: "laravel", svg: "laravel.svg", desde: "2023" },
-  { nombre: "react", svg: "", desde: "2023" },
-  { nombre: "bootstrap", svg: "", desde: "2022" },
-  { nombre: "github", svg: "", desde: "2019" },
-  { nombre: "html", svg: "", desde: "2018" },
-  { nombre: "javascript", svg: "", desde: "2018" },
-  { nombre: "css", svg: "", desde: "2018" },
-  { nombre: "sql", svg: "", desde: "2021" },
-  { nombre: "vuejs", svg: "", desde: "2020" },
+  { nombre: "react", svg: "react.svg", desde: "2023" },
+  { nombre: "bootstrap", svg: "bootstrap.svg", desde: "2022" },
+  { nombre: "github", svg: "github.svg", desde: "2019" },
+  { nombre: "html", svg: "html.svg", desde: "2018" },
+  { nombre: "javascript", svg: "javascript.svg", desde: "2018" },
+  { nombre: "css", svg: "css.svg", desde: "2018" },
+  { nombre: "sql", svg: "sql.svg", desde: "2021" },
+  { nombre: "vuejs", svg: "vuejs.svg", desde: "2020" },
 ];
 
 export default function Tecnologias() {
   return (
     <div>
-      <Typography variant="h4">Tecnologias</Typography>
+      <Typography variant="h3" className="text-rose-700 dark:text-rose-500">Tecnologias</Typography>
       <div className=" flex flex-wrap gap-5 ">
         {tecnologias.map((tec, index) => (
           <div
             key={index}
-            className="flex items-center gap-4 border rounded p-2"
+            className=" flex items-center gap-4 border border-rose-500 text-rose-500 dark:text-rose-600 dark:border-rose-600 dark:bg-rose-200 rounded p-2"
           >
             {tec.svg !== "" && (
               <img
                 src={`/assets/tecnologias/${tec.svg}`}
                 alt={tec.nombre}
-                className="w-12"
+                className="w-12 "
               />
             )}
             <div>
-              <Typography variant="h6">{tec.nombre}</Typography>
-              <Typography variant="small" className="font-normal">
+              <Typography variant="h6" className="uppercase">{tec.nombre}</Typography>
+              <Typography variant="small" className="font-normal text-rose-950 ">
                 Desde: {tec.desde}
               </Typography>
             </div>
